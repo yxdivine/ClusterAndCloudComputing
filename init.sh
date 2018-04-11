845cda043fb8575f3cadb37a1443c92600433dd0
Xinda Yu <xinday@student.unimelb.edu.au>
2018-04-11 12:03:46 +1000

    remove to avoid issues

--- init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ln -s /data/projects/COMP90024/bigInstagram.json
-ln -s /data/projects/COMP90024/mediumInstagram.json
-ln -s /data/projects/COMP90024/tinyInstagram.json
-ln -s /data/projects/COMP90024/melbGrid.json
